dc9a8f9c8b75d47bc1350156e9216cd5017d253f
la163004 <la163004@student.helha.be>
2019-02-13 09:59:35 +0100

    Update of class Deck,better method and more secure with singleton

--- pgroupeA03/src/model/Deck.java
+++ pgroupeA03/src/model/Deck.java
@@ -1,36 +1,61 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Deck {
+	private static Deck instance;
 	private List<Question> questions;
+	
+	public static Deck getInstance() {
+		if(instance == null) {
+			synchronized (Deck.class) {
+				if(instance == null) {
+					instance = new Deck();
+				}
+			}
+		}
+		return instance;
+	}
 
-	//CRUD
-	//Create
-	public Deck(List<Question> questions) {
-		super();
-		this.questions = questions;
+	public Deck() {
+		questions = new ArrayList<>();
 	}
-	//Read
-	public List<Question> getDeck() {
-		return this.questions;
+	
+	public List<Question> getQuestions() {
+		return questions;
 	}
-	//Update
+
 	public void setDeck(List<Question> questions){
 		this.questions=questions;
 	}
-	//Delete
+	
+	public boolean addQuestion(Question x){
+		if(!questions.contains(x)) {
+			questions.add(x);
+			return true;
+		}
+		return false;
+	}
+	
 	public boolean deleteAllDeck(List<Question> x){
 		return questions.removeAll(x);
 	}
+	
 	public boolean deleteSingleQuestion(Question x) {
 		return questions.remove(x);
 	}
-	//ToString
-	public String toString() {
-		return "Deck : [" + questions + "]";
+	
+	public boolean update(Question x) {
+		int ind = getInstance().questions.indexOf(x);
+		if(ind == -1) {
+			return false;
+		}
+		questions.set(ind, x);
+		return true;
 	}
 	
-	
-	
+	public String toString() {
+		return "Deck : [" + questions + "]";
+	}	
 }
