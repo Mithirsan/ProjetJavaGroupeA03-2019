c28c0208d2e2e62200c73a75eb677b635593e918
la163004 <la163004@student.helha.be>
2019-02-13 08:39:38 +0100

    Add basic CRUD on Deck

--- pgroupeA03/src/model/Deck.java
+++ pgroupeA03/src/model/Deck.java
@@ -4,4 +4,28 @@ import java.util.List;
 
 public class Deck {
 	private List<Question> questions;
+
+	//CRUD
+	//Create
+	public Deck(List<Question> questions) {
+		super();
+		this.questions = questions;
+	}
+	//Read
+	public List<Question> getDeck() {
+		return this.questions;
+	}
+	//Update
+	public void setDeck(List<Question> questions){
+		this.questions=questions;
+	}
+	//Delete
+	public void deleteDeck(){
+		this.questions=null;
+	}
+	
+	public String toString() {
+		return "Deck : [" + questions + "]";
+	}	 
+	
 }
