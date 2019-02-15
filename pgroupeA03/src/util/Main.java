130c0a892403c1278bf4868dd043d92800b95485
Axel Lux <Axel Lux@DESKTOP-UFC05BB>
2019-02-15 12:42:47 +0100

    Create a Json work + selection menu for test. I did'nt change the throw in serializable yet

--- pgroupeA03/src/util/Main.java
+++ pgroupeA03/src/util/Main.java
@@ -3,6 +3,7 @@ package util;
 import java.io.File;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.Scanner;
 
 import model.Deck;
 
@@ -10,14 +11,38 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hi");
-		try {
-			Deck.getInstance().loadDeck(new File("Test.json"));		
-			System.out.println(Deck.getInstance());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		System.out.println("Select an option :"+"\n"+"1 : Read a json"+"\n"+"2 : Save a json"+"\n"+"3 :Show the deck"+"\n"+"4 :Exit");
+		Scanner sc = new Scanner (System.in);
+		int input = sc.nextInt();
+		while((input>4)||(input<0)) {
+			System.out.println("ERROR"+"\n"+"Select an option :"+"\n"+"1 : Read a json"+"\n"+"2 : Save a json"+"\n"+"3 :Show the deck"+"\n"+"4 :Exit");
+			input = sc.nextInt();
 		}
-		
+		while(input!=4) {
+				if(input==1) {
+					try {
+						System.out.println("please , enter a name for the file with .json");
+						String str = sc.next();
+						Deck.getInstance().loadDeck(new File(str));
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				else if (input ==2) {
+					try {
+						System.out.println("please , enter a name for the file with .json");
+						String str = sc.next();
+						Serializable.writeDeck(str);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				else if (input ==3) {
+				System.out.println(Deck.getInstance());
+				}
+				System.out.println("Select an option :"+"\n"+"1 : Read a json"+"\n"+"2 : Save a json"+"\n"+"3 :Show the deck"+"\n"+"4 :Exit");
+				input = sc.nextInt();
+		}
+		System.out.println("End of programm");
 	}
-
 }
