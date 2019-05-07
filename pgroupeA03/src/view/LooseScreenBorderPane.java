9982061835e2570637630e01faa74c3ed9b6b845
mithirsan <mithirsan@192.168.1.2>
2019-05-08 01:13:43 +0200

    Loose screen animation pre loaded

--- pgroupeA03/src/view/LooseScreenBorderPane.java
+++ pgroupeA03/src/view/LooseScreenBorderPane.java
@@ -1,25 +1,75 @@
 package view;
 
+import java.io.File;
+
+import javafx.animation.PathTransition;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.VLineTo;
+import javafx.util.Duration;
 
 public class LooseScreenBorderPane extends BorderPane {
 	private Label lblMessage;
+	private PathTransition pathTransition;
+	private ImageView loseImage;
+	private HBox contents;
 	
 	public LooseScreenBorderPane() {
-		HBox text = new HBox();
-		text.getChildren().add(getLblMessage());
-		text.setAlignment(Pos.CENTER);
-		setBottom(text);
+//		setCenter(getContents());
+		HBox txt = new HBox();
+		txt.getChildren().add(getLblMessage());
+		txt.setAlignment(Pos.CENTER);
+		setBottom(txt);
 	}
 
 	public Label getLblMessage() {
 		if(lblMessage == null) {
 			lblMessage = new Label("You've losed the game, click to continue");
 			lblMessage.getStyleClass().add("withe");
+//			lblMessage.setVisible(false);
+			lblMessage.setAlignment(Pos.CENTER);
 		}
 		return lblMessage;
+	}
+
+	public PathTransition getPathTransition() {
+		if(pathTransition == null) {
+			Path path = new Path();
+			path.getElements().add(new MoveTo(480., 0.));
+		    path.getElements().add(new VLineTo(470.));
+		    
+		    getContents().getChildren().add(path);
+		    getContents().getChildren().add(getLoseImage());
+		    
+		    pathTransition = new PathTransition();
+		    pathTransition.setDuration(Duration.seconds(5.0));
+		    pathTransition.setPath(path);
+		    pathTransition.setNode(getLoseImage());
+		    pathTransition.setCycleCount(1);
+		    pathTransition.setOnFinished(e->{
+		    	getLblMessage().setVisible(true);
+		    });
+		}
+		return pathTransition;
+	}
+
+	public ImageView getLoseImage() {
+		if(loseImage == null) {
+			loseImage = new ImageView(new Image(new File("resources/pictures/TheReaper.jpg").toURI().toString()));
+		}
+		return loseImage;
+	}
+
+	public HBox getContents() {
+		if(contents == null) {
+			contents = new HBox();
+		}
+		return contents;
 	}
 }
